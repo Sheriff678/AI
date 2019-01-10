0fb882f1c1c66bbb96d020599e8194d452b53fd2
Sheriff678 <46493944+Sheriff678@users.noreply.github.com>
2019-01-10 10:30:08 -0600

    Update HumanPlayer.java

--- src/com/HumanPlayer.java
+++ src/com/HumanPlayer.java
@@ -1,6 +1,7 @@
 package com;
+import java.util.Scanner;
 
-public abstract class HumanPlayer extends Player
+public class HumanPlayer extends Player
 {
 
     public HumanPlayer(String name, char letter)
@@ -8,13 +9,40 @@ public abstract class HumanPlayer extends Player
         super(name, letter);
     }
 
+    @Override
     public Move getMove(Board board)
     {
+        Scanner input = new Scanner(System.in);
 
+        boolean valid;
+
+        Move move;
+
+        do
+        {
+            System.out.print("Enter coordinates (row col): ");
+            int row = input.nextInt();
+            int col = input.nextInt();
+
+            move = new Move(row, col);
+
+            if(!board.isFull(move) && board.isEmpty(move))
+                valid = true;
+
+            else
+            {
+                valid = false;
+                System.out.println("Invalid mov. Try again.");
+            }
+
+        }while(!valid);
+
+        return move;
     }
 
+    @Override
     public Player freshCopy()
     {
-        
+        return new HumanPlayer(getName(), getLetter());
     }
 }
