53f21610e3e42b9fcfcf6e4ccc9b6ba62028e656
Sheriff678 <46493944+Sheriff678@users.noreply.github.com>
2019-01-10 10:30:29 -0600

    Update RandomComputer.java

--- src/com/RandomComputer.java
+++ src/com/RandomComputer.java
@@ -1,20 +1,42 @@
 package com;
 
-public abstract class RandomComputer extends Player
+
+public class RandomComputer extends Player
 {
     public RandomComputer(String name, char letter)
     {
         super(name, letter);
     }
 
-
+    @Override
     public Move getMove(Board board)
     {
 
+        boolean valid;
+
+        Move move;
+
+        do
+        {
+            int row = (int)(Math.random()*8);
+            int col = (int)(Math.random()*8);
+
+            move = new Move(row, col);
+
+            if(!board.isFull(move) && board.isEmpty(move))
+                valid = true;
+
+            else
+                valid = false;
+
+        }while(!valid);
+
+        return move;
     }
 
+    @Override
     public Player freshCopy()
     {
-
+        return new RandomComputer(getName(), getLetter());
     }
 }
