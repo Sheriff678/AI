87bca93734afb7b072ca5e579b85e49984edf01c
OTHSCS109 <46532795+OTHSCS109@users.noreply.github.com>
2019-01-16 09:59:51 -0600

    Update RandomComputer.java

--- src/com/RandomComputer.java
+++ src/com/RandomComputer.java
@@ -11,26 +11,20 @@ public class RandomComputer extends Player
     @Override
     public Move getMove(Board board)
     {
-
         boolean valid;
-
         Move move;
-
         do
         {
             int row = (int)(Math.random()*8);
             int col = (int)(Math.random()*8);
 
             move = new Move(row, col);
-
-            if(!board.isFull(move) && board.isEmpty(move))
+            if(!board.isFull(move))
                 valid = true;
-
             else
                 valid = false;
 
         }while(!valid);
-
         return move;
     }
 
