f6374ba3a3d7a1d29de2cf5e2a1839133399e1a6
OTHSCS109 <46532795+OTHSCS109@users.noreply.github.com>
2019-01-16 09:58:35 -0600

    Update HumanPlayer.java

--- src/com/HumanPlayer.java
+++ src/com/HumanPlayer.java
@@ -13,26 +13,25 @@ public class HumanPlayer extends Player
     public Move getMove(Board board)
     {
         Scanner input = new Scanner(System.in);
-
         boolean valid;
-
         Move move;
-
         do
         {
-            System.out.print("Enter coordinates (row col): ");
+            System.out.print("Enter row: ");
             int row = input.nextInt();
+            System.out.print("\nEnter column: ");
             int col = input.nextInt();
+            System.out.print("\n");
 
             move = new Move(row, col);
 
-            if(!board.isFull(move) && board.isEmpty(move))
+            if(!board.isFull(move) && ((row >= 0 && row < 8) && (col >= 0 && col < 8)))
                 valid = true;
 
             else
             {
                 valid = false;
-                System.out.println("Invalid mov. Try again.");
+                System.out.println("Invalid move \nTry again.");
             }
 
         }while(!valid);
